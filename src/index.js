68632442731b61c98411379fa4fb994a93f2c99f
Mario <mario@localhost.localdomain>
2020-03-05 10:57:33 +0100

    Initial commit: Webpack and babel

---
+++ src/index.js
@@ -0,0 +1,33 @@
+/////////////
+// IMPORTS //
+/////////////
+
+// BÁSICO
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { BrowserRouter } from 'react-router-dom';
+// import * as serviceWorker from './serviceWorker';
+
+// ESTILOS EXTERNOS
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// COMPONENTES PROPIOS
+// import App from './app/components/App';
+
+//////////////////////////
+// COMPONENTE PRINCIPAL //
+//////////////////////////
+ReactDOM.render(
+  <div>
+    jajaja
+    {/* <BrowserRouter>
+      <App />
+    </BrowserRouter> */}
+  </div>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+				test: /\.jsx?$/,
+				loader: 'babel-loader',
+			},
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+			},
+			{
+				test: /\.(jpe?g|gif|png|svg)$/i,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 10000
+						}
+					}
+				]
+			}
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
